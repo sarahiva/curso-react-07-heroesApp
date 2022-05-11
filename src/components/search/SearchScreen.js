b243ff75af15dda4bd31b792c686f8d259dcb843
Sarahi Vazquez <sara011.svz@gmail.com>
2022-05-11 01:02:49 -0500

    primer commit

---
+++ src/components/search/SearchScreen.js
@@ -0,0 +1,82 @@
+import { useMemo } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+import queryString from 'query-string';
+import { useForm } from '../../hooks/useForm';
+import { getHeroesByName } from '../../selectors/getHeroesByName';
+import { HeroCard } from '../hero/HeroCard';
+
+export const SearchScreen = () => {
+
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const { q = '' } = queryString.parse(location.search);
+
+    const [formValues, handleInputChange] =  useForm({
+        searchText: q
+    });
+
+    const { searchText } = formValues;
+
+    const heroesFilted = useMemo(() => getHeroesByName(q), [q]);
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        console.log(searchText);
+        navigate(`?q=${ searchText }`); //QueryString
+    }
+    
+    return (
+        <>
+            <h1>Búsquedas</h1>
+            <hr />
+
+            <div className="row">
+                <div className="col-5">
+                    <h5>Buscar</h5>
+                    <hr />
+
+                    <form onSubmit={ handleSearch }>
+                        <input
+                            type="text"
+                            placeholder="Buscar un héroe"
+                            className="form-control"
+                            name="searchText"
+                            autoComplete="off"
+                            value={ searchText }
+                            onChange={ handleInputChange }
+                        />
+
+                        <button 
+                            className="btn btn-outline-primary mt-1"
+                            type="submit"
+                        >
+                            Buscar...
+                        </button>
+                    </form>
+                </div>
+
+                <div className="col-7">
+                    <h4>Resultados</h4>
+                    <hr />
+
+                    {
+                        (q === '')
+                            ? <div className="alert alert-info">Buscar un héroe</div>
+                            : ( heroesFilted.length === 0) && <div className="alert alert-danger">No hay resultados: {q}</div>
+                    }
+
+                    {
+                        heroesFilted.map(hero => (
+                            <HeroCard 
+                                key={ hero.id }
+                                { ...hero }
+                            />
+                        ))
+                    }
+
+                </div>
+            </div>
+        </>
+    )
+}
